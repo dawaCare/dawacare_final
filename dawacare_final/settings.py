94ae6c0e4cf3eb170df93cd9d60f869244f272ff
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-08-08 19:44:59 -0400

    renamed allowed host

--- dawacare_final/settings.py
+++ dawacare_final/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'dawa-production.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'dawa-prod.herokuapp.com']
 
 
 # Application definition
