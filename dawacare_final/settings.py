6a5ff1282bd16fee3493faf5c9e117243a807f18
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-08-08 14:17:08 -0400

    deleted secret_key in settings

--- dawacare_final/settings.py
+++ dawacare_final/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e59gfi44wo_bx907nh8@iklscrt2o8o*%z61*!dg5_d@m4mkop'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
