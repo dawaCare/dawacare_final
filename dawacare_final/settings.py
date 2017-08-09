59a37299dda551d65a2673c99a25e6b496a11e84
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-08-08 21:50:14 -0400

    modified settings for whitenoise again

--- dawacare_final/settings.py
+++ dawacare_final/settings.py
@@ -134,7 +134,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
