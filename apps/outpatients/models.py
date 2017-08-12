8bc76235ec36f3eec070902a8652fd926d73f6e2
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-08-11 22:25:11 -0400

    regex for names now allows whitespace

--- apps/outpatients/models.py
+++ apps/outpatients/models.py
@@ -11,7 +11,7 @@ from django.core.validators import RegexValidator
 
 from apps.outpatients import choices
 
-alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+alphanumeric = RegexValidator(r'^[0-9a-zA-Z\s]*$', 'Only alphanumeric characters are allowed.')
 phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 
 
